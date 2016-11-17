0fb492273744482a6ccbdd4b86eb5a58d6f6f8b1
Picard Geoffrey <gepicard@student.42.fr>
2016-11-17 20:17:03 +0100

    zief

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gepicard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/13 14:07:48 by gepicard          #+#    #+#             */
-/*   Updated: 2016/11/17 16:35:36 by gepicard         ###   ########.fr       */
+/*   Updated: 2016/11/17 18:46:58 by geoffrey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,12 @@ char	*ft_first_part(int fd)
 	{
 		buf[ret] = '\0';
 		tmp = ft_strdup(buf);
+		free(dst);
 		dst = ft_strjoin(dst, tmp);
 		free(tmp);
 	}
 	if (!ft_memchr(buf, '\n', BUFF_SIZE))
-		return (dst);
+		return (buf);
 	tmp = ft_strdup(dst);
 	dst = ft_strjoin(tmp, buf);
 	if (buf)
@@ -54,7 +55,7 @@ int     get_next_line(const int fd, char **line)
 	i = 0;
 	buf = ft_strnew(0);
 	src = ft_strnew(0);
-	if (read(fd, buf, 0) == -1 || fd == -1)
+	if (read(fd, buf, 0) == -1 || fd == -1 || !line)
 		return (-1);
 	src = ft_first_part(fd);
 	if (!temp)
@@ -62,15 +63,16 @@ int     get_next_line(const int fd, char **line)
 	src = ft_strjoin(temp, src);
 	if ((ft_memchr(src, '\n', ft_strlen(src))))
 	{
-		while (src[i] != '\n' && src[i])
+		while (src[i] != '\n')
 			i++;
 		*line = ft_strsub(src, 0, i);
 		temp = ft_strsub(src, i + 1, ft_strlen(src));
 		return (1);
 	}
 	*line = ft_strdup(src);
-	if (src[0] != '\0')
+	if ((*line)[0] != '\0')
 	{
+		temp = ft_strnew(0);
 		return (1);
 	}
 	return (0);
